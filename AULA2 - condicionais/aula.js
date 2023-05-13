db655a4fa77b0850e54a7aedd1d420fb18a27022
Luccas <luccasmelim@yahoo.com.br>
2023-05-13 17:52:10 -0300

    Condicionais aplicadas

--- AULA2 - condicionais/aula.js
+++ AULA2 - condicionais/aula.js
@@ -1,7 +1,22 @@
-const numero = 5;
+const numero = 0;
 
-const eNumeroPar =(numero % 5) === 0; // condicional na mesma line
+const numeroDivisivel = (numero % 5) === 0; // condicional na mesma line
 
+/*
 
+= - atribuição (Atribuir um valor para a variavel)
+== - igualdade/comparação (Não compara o tipo do resultado (String, Boolean, Int etc...))
+=== - igualdade/comparação (Compara o tipo do resultado (String, Boolean, Int etc...))
 
-console.log(eNumeroPar);
+*/
+
+if(numero == 0){
+    console.log("Numero inválido!")
+}
+else if(numeroDivisivel){
+    console.log("Sim");
+}
+else{
+    console.log("Não");
+}
+
