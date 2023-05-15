4501b5bb22be3927d9538f1066908e66102d2dff
Luccas <luccasmelim@yahoo.com.br>
2023-05-15 16:23:48 -0300

    att

--- AULA6 - arrays e estruturas de repetição/aula.js
+++ AULA6 - arrays e estruturas de repetição/aula.js
@@ -1,17 +1,4 @@
 
-
-/*
-const alunos = ['Luccas', 'Ana', 'Luis'];
-
-alunos.push('Andre'); // adicionar na array dinamicamente
-alunos[4] = 'Vitor'; // adicionar diretamente na posição, OU, sobrepor na posição desejada.
-alunos.pop(); // apagar o ultimo index
-alunos.shift(); // apagar o primeiro index
-
-console.log(alunos);
-
-*/
-
 const notas = [];
 
 notas.push(5);
