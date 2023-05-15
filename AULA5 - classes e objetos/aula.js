6c7392ff7acf7369346f3eff9ff5f2d2e8562bb5
Luccas <luccasmelim@yahoo.com.br>
2023-05-15 12:57:08 -0300

    classes att2

--- AULA5 - classes e objetos/aula.js
+++ AULA5 - classes e objetos/aula.js
@@ -1,22 +1,21 @@
 class Pessoa{
     nome;
     idade;
+    anoDeNascimento
+
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2023 - idade;
+    }// sempre passará pelo constructor (algo padrão)
 
     descrever(){
         console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
     }
 }
 
-const luccas = new Pessoa();
-luccas.nome = 'Luccas Andre';
-luccas.idade = '19';
+const luccas = new Pessoa('luccas',19);
 
-const ana = new Pessoa();
-ana.nome = 'Ana';
-ana.idade = '17';
+const ana = new Pessoa('ana', 17);
 
-console.log(luccas);
-console.log(ana);
-
-luccas.descrever();
-ana.descrever();
+console.log(luccas)
