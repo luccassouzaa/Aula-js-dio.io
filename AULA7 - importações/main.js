db667edb8cbedbc6e94a671129803032daba11a0
Luccas <luccasmelim@yahoo.com.br>
2023-05-15 22:44:12 -0300

    Aula7 - Closed

--- AULA7 - importações/main.js
+++ AULA7 - importações/main.js
@@ -1,7 +1,32 @@
+/*
+    uma sala contem 5 alunos e para cada aluno foi sorteado um numero de 1 a 100;
+    faça um programa que receba os 5 numeros sorteados para os alunos e mostre o maior numero sorteado
 
+    dados de entrada:
+    5
+    50
+    10
+    98
+    23
 
-const {gets, print} = require('./funcoes-auxiliares');
+    saida
+    98
+*/
 
-print(gets());
+const { gets, print } = require('./funcoes-auxiliares');
 
-// importação com object destroyed
+const numerosSorteados = [];
+
+const quantidadeDeAlunos = gets();
+
+let maiorValorEncontrado = 0;
+
+for (i = 0; i < quantidadeDeAlunos; i++) {
+    const numeroSorteado = gets();
+    numerosSorteados.push(numeroSorteado);
+    if (numeroSorteado > maiorValorEncontrado){
+        maiorValorEncontrado = numeroSorteado;
+    } // estrutura para verificar qual numero é maior.
+}
+
+print(maiorValorEncontrado);
