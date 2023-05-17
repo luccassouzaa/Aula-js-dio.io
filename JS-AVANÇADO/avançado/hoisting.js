80a6a41174678e4bd467c177b9d68e511d090d06
Luccas <luccasmelim@yahoo.com.br>
2023-05-17 14:05:38 -0300

    Hoisting (Içamento)

---
+++ JS-AVANÇADO/avançado/hoisting.js
@@ -0,0 +1,25 @@
+//JavaScript é a base de Içamento
+
+/*
+    Procedural: Ordem importa
+    Içamento: Fazer subir
+*/
+
+
+teste();
+
+function teste(){
+    console.log('testerFunc');
+}
+
+teste();
+
+console.log('----------------------------')
+
+console.log(varTeste1)
+
+var varTeste1 = 'testeVar';
+
+console.log(varTeste1)
+
+console.log('----------------------------')
