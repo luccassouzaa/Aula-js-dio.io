44f1e03ddc648cc74a2ddd6a7317d66ef23877d8
Luccas <luccasmelim@yahoo.com.br>
2023-05-17 12:01:10 -0300

    AttRemovePromise

--- POKEDEX/assets/js/poke-api.js
+++ POKEDEX/assets/js/poke-api.js
@@ -33,15 +33,3 @@ pokeApi.getPokemons = (offset = 0, limit = 2) => {
         .then((detailRequest) => Promise.all(detailRequest)) //aqui estamos esperando todas as requisições terminarem
         .then((pokemonsDetails) => pokemonsDetails)
 }
-
-Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4'),
-    fetch('https://pokeapi.co/api/v2/pokemon/5'),
-    fetch('https://pokeapi.co/api/v2/pokemon/6'),
-
-]).then((results) => {
-    console.log(results)
-})
