6dd4455817cf980fdb916777884a8b1911319c63
Luccas <luccasmelim@yahoo.com.br>
2023-05-17 14:24:13 -0300

    Names

---
+++ JS-AVANÇADO/avançado/nomenclaturas.js
@@ -0,0 +1,6 @@
+/*
+    Convenções com nomes:
+    var _teste = 10; //quando ela é restrita a algum escopo(nao sera alterada fora do escolo que ela foi definida)
+    let $teste = 10; //jQuery para varaiveis
+    const letra = 10; // Class ou Função Construtora, podemos começar com letra maiuscula, caso seja alguma variavel, usar sintax do camelCase, se a varaivel for importante, usar tudo em MAIUSCULO
+*/
